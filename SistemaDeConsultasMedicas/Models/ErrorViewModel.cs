6ca8749b0cc8c805807ae30c77321a89cb38dae2
EnriqueFabianPM <perezmedellinenriquefabian@gmail.com>
2025-01-11 16:49:15 -0600

    Add project files.

---
+++ SistemaDeConsultasMedicas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaDeConsultasMedicas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
