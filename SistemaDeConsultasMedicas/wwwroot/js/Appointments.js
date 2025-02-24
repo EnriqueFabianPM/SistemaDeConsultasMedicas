6add7e9286ba2fccf294b1e399ee1cf535f816a3
Elias <21017@virtual.utsc.edu.mx>
2025-02-23 18:31:57 -0600

    Se creó un archivo con los servicios de la vista de Appointments
    se agregaron los siguientes métodos:
    -Appointments
    -Municipalities
    -Consultories
    -Doctors
    
    también se agregaron los respectivos viewmodels para cada uno de los métodos

--- SistemaDeConsultasMedicas/wwwroot/js/Appointments.js
+++ SistemaDeConsultasMedicas/wwwroot/js/Appointments.js
@@ -17,8 +17,19 @@ const app = createApp({
              estos ejemplos se pueden eliminar para cambiarlos 
              por los que sean necesarios, solo son ejemplos
             */
-            Message: 'Hola desde modelo de Vue',
-            Array: [],
+            notes: '',
+            municipality: {
+                Id_Municipality: 0,
+                Name: '',
+                Zip_Code: 0,
+            },
+            consultory: {
+                Id_Municipality: 0,
+                Name: '',
+                Zip_Code: 0,
+            },
+            municipalities: [],
+            consultories: [],
             Object: {},
             Int: 0,
             boolean: false,
@@ -27,7 +38,6 @@ const app = createApp({
     methods: {
         //Aquí se crearán los métodos js
         createUser() {
-
             axios.post(createUser, {
 
             })
@@ -38,6 +48,46 @@ const app = createApp({
                 .catch(error => {
                     console.log(error, 'Mensaje de error')
                 });
+        },
+
+        getMunicipalities() {
+            axios.get(createUser)
+                .then(response => {
+
+                    this.municipalities = response.data;
+
+                    console.log(response.data, 'respuesta del método');
+                })
+                .catch(error => {
+                    console.log(error, 'Mensaje de error')
+                });
+        },
+
+        //Método que se ejecuta cuando seleccionamos otro municipio
+        onMunicipalityChange(municipality) {
+
+            //Igualar el modelo de vue a los valores del objeto seleccionado
+            this.municipality = {
+                Id_Municipality: municipality.Id_Municipality,
+                Name: municipality.Name,
+                Zip_Code: municipality.Zip_Code,
+            };
+
+            this.getConsultories(municipality.Id_Municipality);
+        },
+
+        getConsultories(Id_Municipality) {
+            axios.get(getConsultories)
+                .then(response => {
+
+                    this.consultories = response.data;
+
+                    console.log(response.data, 'respuesta del método');
+                })
+                .catch(error => {
+                    console.log(error, 'Mensaje de error')
+                });
+
         },
     },
     mounted() {
